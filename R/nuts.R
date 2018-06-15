511cec11be8eaf1ec9e4fa76e1dd2107fb007303
Simon Wotherspoon <Simon.Wotherspoon@utas.edu.au>
2018-06-15 11:57:19 +1000

    Code cleanup
    Tidy code format

--- R/nuts.R
+++ R/nuts.R
@@ -23,33 +23,34 @@
 ##'   adaptively setting path lengths in Hamiltonian Monte
 ##'   Carlo. Journal of Machine Learning Research, 15(1), 1593-1623.
 ##' @export
-NUTS <- function(theta, f, grad_f, n_iter, M_diag = NULL, M_adapt = 50, delta = 0.5, max_treedepth = 10, eps = 1, verbose = TRUE){
+NUTS <- function(theta, f, grad_f, n_iter, M_diag=NULL, M_adapt=50, delta=0.5, max_treedepth=10, eps=1, verbose=TRUE){
+
   theta_trace <- matrix(0, n_iter, length(theta))
-  par_list <- list(M_adapt = M_adapt)
+  par_list <- list(M_adapt=M_adapt)
+
   for(iter in 1:n_iter){
-    nuts <- NUTS_one_step(theta, iter, f, grad_f, par_list, delta = delta, max_treedepth = max_treedepth, eps = eps, verbose = verbose)
+    nuts <- NUTS_one_step(theta, iter, f, grad_f, par_list, delta=delta, max_treedepth=max_treedepth, eps=eps, verbose=verbose)
     theta <- nuts$theta
     par_list <- nuts$pars
     theta_trace[iter, ] <- theta
   }
+
   theta_trace
 }
 
 
-##' @importFrom stats rnorm
-NUTS_one_step <- function(theta, iter, f, grad_f, par_list, delta = 0.5, max_treedepth = 10, eps = 1, verbose = TRUE){
+##' @importFrom stats rnorm runif
+NUTS_one_step <- function(theta, iter, f, grad_f, par_list, delta=0.5, max_treedepth=10, eps=1, verbose=TRUE){
+
   kappa <- 0.75
   t0 <- 10
   gamma <- 0.05
   M_adapt <- par_list$M_adapt
-  if(is.null(par_list$M_diag)){
-    M_diag <- rep(1, length(theta))
-  } else{
-    M_diag <- par_list$M_diag
-  }
+  M_diag <-  par_list$M_diag
+  if(is.null(M_diag)) M_diag <- rep(1, length(theta))
 
-  if(iter == 1){
-    eps <- find_reasonable_epsilon(theta, f, grad_f, M_diag, eps = eps, verbose = verbose)
+  if(iter == 1) {
+    eps <- find_reasonable_epsilon(theta, f, grad_f, M_diag, eps=eps, verbose=verbose)
     mu <- log(10*eps)
     H <- 0
     eps_bar <- 1
@@ -61,7 +62,7 @@ NUTS_one_step <- function(theta, iter, f, grad_f, par_list, delta = 0.5, max_tre
   }
 
   r0 <- rnorm(length(theta), 0, sqrt(M_diag))
-  u <- runif(1, 0, exp(f(theta) - 0.5 * sum(r0**2 / M_diag)))
+  log <- u <- joint_log_density(theta, r0, f, M_diag) - rexp(1)
   if(is.nan(u)){
     warning("NUTS: sampled slice u is NaN")
     u <- runif(1, 0, 1e5)
@@ -70,12 +71,12 @@ NUTS_one_step <- function(theta, iter, f, grad_f, par_list, delta = 0.5, max_tre
   theta_plus <- theta
   r_minus <- r0
   r_plus <- r0
-  j=0
-  n=1
-  s=1
-  if(iter > M_adapt){
+  j <- 0
+  n <- 1
+  s <- 1
+  if(iter > M_adapt)
     eps <- runif(1, 0.9*eps_bar, 1.1*eps_bar)
-  }
+
   while(s == 1){
     # choose direction {-1, 1}
     direction <- sample(c(-1, 1), 1)
@@ -90,27 +91,26 @@ NUTS_one_step <- function(theta, iter, f, grad_f, par_list, delta = 0.5, max_tre
     }
     if(is.nan(temp$s)) temp$s <- 0
     if(temp$s == 1){
-      if(runif(1) < temp$n / n){
+      if(runif(1) < temp$n/n)
         theta <- temp$theta
-      }
     }
-    n <- n + temp$n
+    n <- n+temp$n
     s <- check_NUTS(temp$s, theta_plus, theta_minus, r_plus, r_minus)
-    j <- j + 1
+    j <- j+1
     if(j > max_treedepth){
       warning("NUTS: Reached max tree depth")
       break
     }
   }
   if(iter <= M_adapt){
-    H <- (1 - 1/(iter + t0))*H + 1/(iter + t0) * (delta - temp$alpha / temp$n_alpha)
-    log_eps <- mu - sqrt(iter)/gamma * H
-    eps_bar <- exp(iter**(-kappa) * log_eps + (1 - iter**(-kappa)) * log(eps_bar))
+    H <- (1 - 1/(iter+t0))*H+1/(iter+t0)*(delta - temp$alpha/temp$n_alpha)
+    log_eps <- mu - sqrt(iter)/gamma*H
+    eps_bar <- exp(iter^(-kappa)*log_eps+(1 - iter^(-kappa))*log(eps_bar))
     eps <- exp(log_eps)
   } else{
     eps <- eps_bar
   }
 
-  return(list(theta = theta,
-              pars = list(eps = eps, eps_bar = eps_bar, H = H, mu = mu, M_adapt = M_adapt, M_diag = M_diag)))
+  return(list(theta=theta,
+              pars=list(eps=eps, eps_bar=eps_bar, H=H, mu=mu, M_adapt=M_adapt, M_diag=M_diag)))
 }
